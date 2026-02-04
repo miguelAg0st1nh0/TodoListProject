aec9e716796805bd269117f6db2fc946bd32e7ef
Miguel Agostinho <miguel1995jordao@icloud.com>
2026-02-04 17:27:57 +0000

    Layed out the foundation to work on the home page, preparing next to add default tasks to it.

---
+++ TodoListProject/Models/TaskClass.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoListProject.Models
+{
+    public class TaskClass
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
